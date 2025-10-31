d972a158c68e383ce424b66acd9e484fd569f60d
김상윤 <ksy151020@gmail.com>
2025-10-31 23:16:55 +0900

    feat: Lotto 클래스 기능 추가 및 단위 테스트 작성

--- src/Contants.js
+++ src/Contants.js
@@ -13,9 +13,9 @@ export const PRIZE = {
 }
 
 export const ERROR_MESSAGE = {
-    AMOUNT_ERROR: '[ERROR] 구입 금액은 1000원 단위 양수여야 합니다',
-    NUMBER_RANGE_ERROR: '[ERROR] 로또 번호는 1~45 사이여야 합니다',
-    NUMBER_COUNT_ERROR: '[ERROR] 로또 번호는 6개여야 합니다',
-    DUPLICATE_ERROR: '[ERROR] 중복된 로또 번호가 존재합니다',
-    INVALID_INPUT_ERROR: '[ERROR] 잘못된 입력값 입니다'
+    AMOUNT_ERROR: '[ERROR] 구입 금액은 1000원 단위 양수여야 합니다.',
+    NUMBER_RANGE_ERROR: '[ERROR] 로또 번호는 1~45 사이여야 합니다.',
+    NUMBER_COUNT_ERROR: '[ERROR] 로또 번호는 6개여야 합니다.',
+    DUPLICATE_ERROR: '[ERROR] 중복된 로또 번호가 존재합니다.',
+    INVALID_INPUT_ERROR: '[ERROR] 잘못된 입력값 입니다.'
 }
